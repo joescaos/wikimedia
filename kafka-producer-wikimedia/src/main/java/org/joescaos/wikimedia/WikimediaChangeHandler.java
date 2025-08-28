24d4d39c2d2dd2517f7cf34b7cfae0f6c7c29a7b
Johan Esteban Cañas <johanestebancanas@192.168.1.16>
2025-08-27 20:58:15 -0500

    first commit

---
+++ kafka-producer-wikimedia/src/main/java/org/joescaos/wikimedia/WikimediaChangeHandler.java
@@ -0,0 +1,50 @@
+package org.joescaos.wikimedia;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.MessageEvent;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class WikimediaChangeHandler implements EventHandler {
+
+    private final KafkaProducer<String, String> producer;
+    private final String topic;
+    private static final Logger logger = LoggerFactory.getLogger(WikimediaChangeHandler.class.getName());
+
+
+
+    public WikimediaChangeHandler(KafkaProducer<String, String> producer, String topic) {
+        this.producer = producer;
+        this.topic = topic;
+    }
+
+    @Override
+    public void onOpen() {
+
+    }
+
+    @Override
+    public void onClosed() {
+        producer.close();
+    }
+
+    @Override
+    public void onMessage(String s, MessageEvent messageEvent) {
+        logger.info("Message: {}", messageEvent.getData());
+        producer.send(new ProducerRecord<>(topic, messageEvent.getData()));
+
+    }
+
+    @Override
+    public void onComment(String s) {
+
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        logger.error("Error in Stream Reading: ", throwable);
+    }
+}
