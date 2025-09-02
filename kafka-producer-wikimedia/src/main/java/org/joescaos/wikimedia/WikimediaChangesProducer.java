0b28674c3f2512144248ea0dfa7b2956749e6c1f
Johan Esteban Cañas <johanestebancanas@192.168.1.16>
2025-09-01 22:07:15 -0500

    producer improvement for low latency and high throughput

--- kafka-producer-wikimedia/src/main/java/org/joescaos/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/org/joescaos/wikimedia/WikimediaChangesProducer.java
@@ -2,11 +2,13 @@ package org.joescaos.wikimedia;
 
 import com.launchdarkly.eventsource.EventHandler;
 import com.launchdarkly.eventsource.EventSource;
+import okhttp3.Headers;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.net.URI;
+import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -21,13 +23,22 @@ public class WikimediaChangesProducer {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        // high throughput configs
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, String.valueOf(32 * 1034));
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
         String topic = "wikimedia.recentchange";
 
         EventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
+        Headers headers = new Headers.Builder()
+                .add("User-Agent", "my-kafka-producer/1.0 (tu-email@ejemplo.com)")
+                .build();
         String url = "http://stream.wikimedia.org/v2/stream/recentchange";
-        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url))
+                .headers(headers);
         try (EventSource eventSource = builder.build()) {
 
             // start producer in another thread
